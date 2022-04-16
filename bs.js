12226057ff2b0b0b9b8f02cd820ee4fed97db17f
k58324762t <wirdjsgt@gmail.com>
2022-04-16 14:56:38 +0600

    first commit

---
+++ bs.js
@@ -0,0 +1,23 @@
+const logBtn = document.getElementById('get-text');
+const result = document.getElementById('result')
+logBtn.addEventListener('click', fetchData);
+
+async function fetchData() {
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+
+    data.forEach(obj => {
+        Object.entries(obj).forEach(([name, username]) => {
+
+        result.innerHTML+=  
+           ` <ol>
+                <li>${obj.name}</li>
+                <li>${obj.username}</li>
+                --------------------
+
+            </ol>`
+        });
+        
+    });
+}
